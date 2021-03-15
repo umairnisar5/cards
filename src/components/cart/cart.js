526d39b5ba362166060cc346cc47e057aedaa556
umairnisar5 <49435408+umairnisar5@users.noreply.github.com>
2021-03-15 13:28:25 +0500

    create a popup on cart list

--- src/components/cart/cart.js
+++ src/components/cart/cart.js
@@ -12,7 +12,13 @@ import history from "../../history";
 import LocalGroceryStoreIcon from "@material-ui/icons/LocalGroceryStore";
 import LocalMallIcon from "@material-ui/icons/LocalMall";
 import AddIcon from "@material-ui/icons/Add";
-
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { useTheme } from '@material-ui/core/styles';
 import RemoveIcon from "@material-ui/icons/Remove";
 import Typography from "@material-ui/core/Typography";
 import { connect } from "react-redux";
@@ -51,6 +57,17 @@ let handleQuantity = (type, value) => {
 
 function Cart(props) {
   const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
   const [searchTerm, setSearchTerm] = useState("");
   return (
     <div>
@@ -96,15 +113,6 @@ function Cart(props) {
                 </CardContent>
               </CardActionArea>
               <CardActions>
-                {/* <Button
-                  className="Add_item"
-                  id="add_cart_btn"
-                  size="small"
-                  color="primary"
-                  onClick={() => handleQuantity("+", val)}
-                >
-                  +
-                </Button> */}
                 <AddIcon
                   className="Add_item"
                   id="add_cart_btn"
@@ -132,7 +140,7 @@ function Cart(props) {
                   color="secondary"
                   className={classes.button}
                   startIcon={<DeleteIcon />}
-                >
+                > 
                   Delete Item
                 </Button>
               </CardActions>
@@ -140,35 +148,58 @@ function Cart(props) {
           );
         })}
       </div>
-      <div className="chcek_item">
-        <h1 className="summary">Summary </h1>
-        <div className="main_div" >
-        <h2>Total Items: {props.totalItems} </h2>
-        <h2>Total Price {`$${props.totalPrice}`} </h2>
-        </div>
-        <button className="button_chcek">
-          Chcek out
-        </button>
-      </div>
+      { props.cartList.length != 0 ? (
+      <div>
+        <Button id="button" variant="outlined" color="primary" onClick={handleClickOpen}>
+          Check Out
+        </Button>
+        <Dialog
+          fullScreen={fullScreen}
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="responsive-dialog-title"
+          id="dialog"
+        >
+          <DialogTitle id="responsive-dialog-title">Cart Summary</DialogTitle>
+          <DialogContent>
+            <div className="item_number">
+              <h3>Total Items:</h3>
+              <h3>{props.totalItems}</h3>
+            </div>
+            <div className="item_number">
+              <h3>Total Price:</h3>
+              <h3> {`$${props.totalPrice}`} </h3>
+            </div>
+          </DialogContent>
+          <DialogActions>
+          <Button onClick={handleClose} color="primary" id="cancel_dialog">
+              Cancel
+            </Button>
+            <Button onClick={handleClose} color="primary" id="pay_dialog">
+              Proceed To Pay
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>) : (
+        <h1> </h1>
+      )}
     </div>
   );
 }
 
 const mapStateToProps = (state) => {
-  let totalItems  = 0;
+  let totalItems = 0;
   let totalPrice = 0;
-  state.cartList.map((item) =>{
-totalItems += item.quantity;
-totalPrice += item.quantity  * item.price;
-  })
+  state.cartList.map((item) => {
+    totalItems += item.quantity;
+    totalPrice += item.quantity * item.price;
+  });
   return {
     cartList: state.cartList,
     cartValue: state.cartValue,
     totalItems: totalItems,
-    totalPrice: totalPrice
+    totalPrice: totalPrice,
   };
 };
-
-
 
 export default connect(mapStateToProps)(Cart);
